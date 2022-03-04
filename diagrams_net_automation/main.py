b75238a330bbf3123486e4c993789e3e40869984
Patrick Stöckle <patrick.stoeckle@tum.de>
2022-03-04 17:29:57 +0100

    :sparkles: Flag for ommiting different types

--- diagrams_net_automation/main.py
+++ diagrams_net_automation/main.py
@@ -7,7 +7,7 @@ from json import dumps, loads
 from logging import INFO, basicConfig, getLogger
 from pathlib import Path
 from subprocess import call
-from typing import List, MutableMapping
+from typing import List, MutableMapping, Sequence
 
 from tqdm import tqdm
 
@@ -68,13 +68,30 @@ def convert_diagrams(
     ),
     width: List[int] = Option([], "--width", "-w"),
     include_xml: bool = Option(False, "--include-xml", "-X", is_flag=True),
+    create_png: bool = Option(False, "--generate-png", "-P", is_flag=True),
+    create_jpg: bool = Option(False, "--generate-jpg", "-J", is_flag=True),
+    skip_pdf: bool = Option(False, "--skip-pdf-generation", "-S", is_flag=True),
 ) -> None:
     """
     Converts Draw.io files to PDF and PNG.
     """
     _setup_output_directory(output_directory_path)
-    cache_file = input_directory_path.joinpath(CACHE_FILE)
-    content = _load_cache(cache_file)
+
+    converter = Converter(
+        cache_file=input_directory_path.joinpath(CACHE_FILE),
+        create_jpg=create_jpg,
+        create_png=create_png,
+        draw_io=draw_io,
+        output_directory=output_directory_path,
+        skip_pdf=skip_pdf,
+        widths=width,
+    )
+    if not create_jpg:
+        echo("We will generate no JPGs.")
+    if not create_png:
+        echo("We will generate no PNGs.")
+    if skip_pdf:
+        echo("We will generate no PDFs.")
     files = sorted(
         list(
             chain(
@@ -86,99 +103,147 @@ def convert_diagrams(
         )
     )
     for file in tqdm(files):
-        _convert_file(content, draw_io, file, output_directory_path, width)
-    cache_file.write_text(dumps(content))
+        converter.convert_file(file)
 
 
-def _convert_file(
-    cache_content: MutableMapping[str, str],
-    draw_io: Path,
-    file: Path,
-    output_directory_path: Path,
-    widths: List[int],
-) -> None:
-    file_hash = hash_file(file)
-    if str(file) in cache_content.keys() and cache_content[str(file)] == file_hash:
-        _LOGGER.info(f"The file {file} has not changed since the last conversion.")
-        return
-    echo(f"Converting {file}")
-    output_subdir = output_directory_path.joinpath(file.parent)
-    if not output_subdir.is_dir():
-        output_subdir.mkdir(parents=True)
-    new_pdf_file = output_subdir.joinpath(file.stem + ".pdf")
-    new_png_file = output_subdir.joinpath(file.stem + ".png")
-    new_jpg_file = output_subdir.joinpath(file.stem + ".jpg")
-    _LOGGER.info(f"Convert {file} to PDF")
-    call(
-        [
-            draw_io,
-            file,
-            "--export",
-            "--output",
-            new_pdf_file,
-            "--crop",
-        ]
-    )
-    _LOGGER.info(f"Convert {file} to PNG")
-    call(
-        [
-            draw_io,
-            file,
-            "--export",
-            "--output",
-            new_png_file,
-            "--transparent",
-        ]
-    )
-    _LOGGER.info(f"Convert {file} to JPEG")
-    call(
-        [
-            draw_io,
-            file,
-            "--export",
-            "--output",
-            new_jpg_file,
-            "--transparent",
-        ]
-    )
-    for width in widths:
-        _LOGGER.info(f"Convert {file} to PNG with width {width}")
-        new_width_png_file = output_directory_path.joinpath(f"{file.stem}_{width}.png")
-        call(
-            [
-                draw_io,
-                file,
-                "--export",
-                "--output",
-                new_width_png_file,
-                "--width",
-                str(width),
-                "--transparent",
-            ]
-        )
-        _LOGGER.info(f"Convert {file} to JPEG with width {width}")
-        new_width_jpg_file = output_directory_path.joinpath(f"{file.stem}_{width}.jpg")
-        call(
-            [
-                draw_io,
-                file,
-                "--export",
-                "--output",
-                new_width_jpg_file,
-            ]
-        )
-    cache_content[str(file)] = file_hash
-    echo(f"Converting {file}: done!")
+class Converter(object):
+    """
+    Converts Draw.io files.
+    """
 
+    draw_io: Path
+    cache_file: Path
+    output_directory: Path
+    widths: Sequence[int]
+    create_png: bool
+    create_jpg: bool
+    skip_pdf: bool
 
-def _load_cache(cache_file: Path) -> MutableMapping[str, str]:
-    content: MutableMapping[str, str]
-    if cache_file.is_file():
-        content = loads(cache_file.read_text())
-        return content
-    else:
-        content = {}
-    return content
+    _cache: MutableMapping[str, str]
+
+    def __init__(
+        self,
+        draw_io: Path,
+        cache_file: Path,
+        output_directory: Path,
+        widths: Sequence[int],
+        create_png: bool,
+        create_jpg: bool,
+        skip_pdf: bool,
+    ) -> None:
+        self.draw_io = draw_io
+        self.cache_file = cache_file
+        self.output_directory = output_directory
+        self.widths = widths
+        self.create_jpg = create_jpg
+        self.create_png = create_png
+        self.skip_pdf = skip_pdf
+        if self.cache_file.is_file():
+            self._cache = loads(cache_file.read_text())
+        else:
+            self._cache = {}
+
+    def convert_file(
+        self,
+        file: Path,
+    ) -> None:
+        """
+        Convert a file.
+        """
+        file_hash = hash_file(file)
+        if str(file) in self._cache.keys() and self._cache[str(file)] == file_hash:
+            _LOGGER.info(f"The file {file} has not changed since the last conversion.")
+            return
+        echo(f"Converting {file}")
+        output_subdir = self.output_directory.joinpath(file.parent)
+        if not output_subdir.is_dir():
+            output_subdir.mkdir(parents=True)
+        new_pdf_file = output_subdir.joinpath(file.stem + ".pdf")
+        new_png_file = output_subdir.joinpath(file.stem + ".png")
+        new_jpg_file = output_subdir.joinpath(file.stem + ".jpg")
+        if self.skip_pdf:
+            _LOGGER.info(f"PDF conversion skipped!")
+        else:
+            _LOGGER.info(f"Convert {file} to PDF")
+            call(
+                [
+                    str(self.draw_io),
+                    file,
+                    "--export",
+                    "--output",
+                    new_pdf_file,
+                    "--crop",
+                ]
+            )
+        if self.create_png:
+            _LOGGER.info(f"Convert {file} to PNG")
+            call(
+                [
+                    str(self.draw_io),
+                    file,
+                    "--export",
+                    "--output",
+                    new_png_file,
+                    "--transparent",
+                ]
+            )
+        else:
+            _LOGGER.info(f"PNG conversion skipped!")
+        if self.create_jpg:
+            _LOGGER.info(f"Convert {file} to JPEG")
+            call(
+                [
+                    str(self.draw_io),
+                    file,
+                    "--export",
+                    "--output",
+                    new_jpg_file,
+                    "--transparent",
+                ]
+            )
+        else:
+            _LOGGER.info(f"JPG conversion skipped!")
+        for width in self.widths:
+            if self.create_png:
+                _LOGGER.info(f"Convert {file} to PNG with width {width}")
+                new_width_png_file = self.output_directory.joinpath(
+                    f"{file.stem}_{width}.png"
+                )
+                call(
+                    [
+                        str(self.draw_io),
+                        file,
+                        "--export",
+                        "--output",
+                        new_width_png_file,
+                        "--width",
+                        str(width),
+                        "--transparent",
+                    ]
+                )
+            if self.create_jpg:
+                _LOGGER.info(f"Convert {file} to JPEG with width {width}")
+                new_width_jpg_file = self.output_directory.joinpath(
+                    f"{file.stem}_{width}.jpg"
+                )
+                call(
+                    [
+                        str(self.draw_io),
+                        file,
+                        "--export",
+                        "--output",
+                        new_width_jpg_file,
+                    ]
+                )
+        if self.create_jpg or self.create_png or not self.skip_pdf:
+            self._cache[str(file)] = file_hash
+        echo(f"Converting {file}: done!")
+
+    def update_cache(self) -> None:
+        """
+        Store the current cache in the cache file.
+        """
+        self.cache_file.write_text(dumps(self._cache))
 
 
 def _setup_output_directory(output_directory_path: Path) -> None:
